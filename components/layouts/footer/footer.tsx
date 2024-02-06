f2d0c5e946a462c826e7b4b86555b801fd6501b1
Арапбай Абдужапаров <aabdujaparov@it.gov.kg>
2024-02-07 01:27:52 +0600

    added simple header, footer. Installed 1 package, downloaded 2 image

--- components/layouts/footer/footer.tsx
+++ components/layouts/footer/footer.tsx
@@ -1,3 +1,16 @@
+
+
 export default function Footer() {
-  return <div>footer</div>;
+  return (
+    <div className="h-[5rem]  flex items-center px-28 border-t-2 border-gray-200 justify-between">
+      <p>
+        author
+      </p>
+      <a href="tel:+996 999 999 999">
+        <p>
+          call
+        </p>
+      </a>
+    </div>
+  )
 }
--- components/layouts/header/header.tsx
+++ components/layouts/header/header.tsx
@@ -1,5 +1,34 @@
 "use client";
 
+import logo from "../../../public/voice-logo.png"
+import userLogo from "../../../public/avatar.png"
+import Image from "next/image";
+import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Button, DropdownSection } from "@nextui-org/react";
+
 export default function Header() {
-  return <div>header</div>;
+  return (
+    <div className="h-[4rem] bg-slate-100 flex items-center px-28 border-b-2 border-gray-400 justify-between">
+      <Button>
+        <Image src={logo} alt={"logo"} width={50} />
+      </Button>
+      <div>
+        <Dropdown>
+          <DropdownTrigger>
+            {userLogo ?
+              <Button className="bg-zinc-300 rounded-full w-10 h-10 flex items-center justify-center">
+                <Image src={userLogo} alt={"logo"} />
+              </Button> :
+              <Button variant="bordered">menu</Button>
+            }
+          </DropdownTrigger>
+          <DropdownMenu aria-label="Static Actions">
+            <DropdownSection title="mail" className="text-sm bg-slate-50 p-2 rounded-md border-2">
+              <DropdownItem key="profile" className="cursor-pointer">profile</DropdownItem>
+              <DropdownItem key="logOut" className="cursor-pointer">log-out</DropdownItem>
+            </DropdownSection>
+          </DropdownMenu>
+        </Dropdown>
+      </div>
+    </div>
+  );
 }
