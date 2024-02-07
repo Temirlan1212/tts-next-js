c02e31daec93a5572b58de1e34878845f1e3a958
Temirlan Kadyrov <ltemirlan2003@gmail.com>
2024-02-07 12:33:58 +0600

    add: footer and header

--- components/layouts/footer/footer.tsx
+++ components/layouts/footer/footer.tsx
@@ -1,3 +1,12 @@
 export default function Footer() {
-  return <div>footer</div>;
+  return (
+    <div className="border border-slate-200">
+      <div className="container h-[5rem] flex items-center  justify-between">
+        <p>author</p>
+        <a href="tel:+996 999 999 999">
+          <p>call</p>
+        </a>
+      </div>
+    </div>
+  );
 }
--- components/layouts/header/header.tsx
+++ components/layouts/header/header.tsx
@@ -1,5 +1,21 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import logo from "@/public/voice-logo.png";
+import { ProfileCombox } from "./profile-combox";
+
 export default function Header() {
-  return <div>header</div>;
+  return (
+    <div className="border border-slate-200">
+      <div className="container flex items-center h-[4rem] justify-between">
+        <Button variant="ghost">
+          <Image src={logo} alt={"logo"} width={50} />
+        </Button>
+        <div>
+          <ProfileCombox />
+        </div>
+      </div>
+    </div>
+  );
 }
